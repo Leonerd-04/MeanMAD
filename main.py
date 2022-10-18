3fe970c650f3f6197a4e4b038795fcd807e9221e
leonard04 <leonerd04.2718@gmail.com>
2022-10-18 13:39:52 -0700

    Added a help command

--- main.py
+++ main.py
@@ -24,11 +24,25 @@ def parse_command(raw: str, commands: dict[str, Callable]) -> Callable:
     return lambda data: data.extend(parse_data(raw.split(' ')))
 
 
+def print_help(commands: dict[str, str]) -> None:
+    for command, help in commands.items():
+        print(f"{command} \t----\t {help}")
+
+
 def main() -> None:
     nums = []
+
+    command_help = {
+        "help": "Prints this menu",
+        "eval": "Evaluates the mean and MAD for the current data set",
+        "clear": "Clears the data set",
+        "<data>": "Adds the inputted data to the current set"
+    }
+
     commands = {
         "eval": lambda data: eval_mean_mad(data),
-        "clear": lambda data: data.clear()
+        "clear": lambda data: data.clear(),
+        "help": lambda data: print_help(command_help)
     }
 
     raw = input("Enter a command or raw data: ")
